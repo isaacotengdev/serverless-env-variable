9ac3b5f2d8885adf556837ece37235133e3a1a53
isaacotengdev <isaacnewtonoteng@gmail.com>
2024-11-16 23:33:23 +0000

    first commit

---
+++ handler.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+
+def hello(event, context):
+    
+    return os.environ['FIRST_NAME']
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
